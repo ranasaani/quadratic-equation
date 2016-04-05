1efeb83f1631534d814db9748645c0fdee219454
Muhammad Usman <ranasaani@gmail.com>
2016-04-05 13:19:17 +0500

    ajax

---
+++ QuadraticEquation.UserInterface.php
@@ -0,0 +1,54 @@
+
+<html>
+<head>
+<title>Quadratic Equation Solver</title>
+
+</head>
+<body style="background:#2F4F2F;">
+<table border="1" align="center" style="width:800px;margin-top:60px;font-size:1.4em">
+<tr><td style="background:#CFD784;text-align:center;height:50px;">
+<h2>Equation Solver for  ax<sup>2</sup>+bx+c = 0</h2>
+</td></tr><tr>
+<td valign="top" style="background:#E0EEE0;padding-left:20px;padding-top:10px;height:300px;">
+<form method="POST" action="">
+a = <input type="text" name="a" value="1" maxlength="10" size="10">
+ &nbsp;&nbsp;&nbsp; b= <input type="text" name="b" value="2"  maxlength="10" size="10">
+&nbsp;&nbsp;&nbsp; c = <input type="text" name="c" value="1" maxlength="10" size="10">
+&nbsp;Significant Figures = <input type="text" name="dec" value="2" maxlength="6" size="6">
+<input type="hidden" name="seen" value="t">
+<input type="submit" value="Solve">
+</form><br/>
+<div id='roots' style="display:none">
+<h4>Roots</h4>
+<div id="answer"></div>
+</div>
+
+
+</td></tr></table>
+</body>
+<script src='jquery.js'> </script>
+<script>
+
+	var form = $('form');
+	form.on('submit', function(e){
+		e.preventDefault();
+		var data = form.serialize();
+
+		$.ajax({
+		  url: 'QuadraticEquation.class.php',
+		  data: data,
+		  success: function(response){
+			response= JSON.parse(response);
+			var div = $('#roots');
+			div.show()
+
+			$(div.find('#answer')).html('('+response.answer[0]+', '+response.answer[1]+')')
+
+			}
+		});
+
+	})
+
+</script>
+</html>
+
---
+++ QuadraticEquation.class.php
@@ -0,0 +1,117 @@
+<?php
+ /***************************************************
+//* Quadratic Equation Solver                       *
+//* Version:	  2.0                               *
+//* Release:      2010-10-12                        *
+//* Author:       Intigam Mammadov                  *
+//* Country:      Azerbaijan                        *
+//* Contact:      php.mysql.pr@gmail.com            *
+//* Copyright:    free for non-commercial use .     *
+//* Any suggestion, request or bug, contact me!     *
+//***************************************************/
+/*This class calculates  roots of  quadratic equation with real coefficients*/
+
+class equation2 {
+   public $p=array();
+   public $x=array();
+   public $dec;
+    
+   public function __construct($a,$b,$c,$dec) {
+	
+             if($dec==""){$dec="2";}
+             if(isset($a) && $a!=""){
+                $this->p['a']=(float)$a;
+                $this->p['b']=(float)$b;
+                $this->p['c']=(float)$c;
+                $this->dec=(int)$dec;
+                }
+             }//construct
+  private  function D(){
+            if($this->p['a']!=0){
+               $delta=pow($this->p['b'],2)-4*$this->p['a']*$this->p['c'];
+            return $delta;
+            }else{$this->root();}
+		         }//D
+   public function checkD(){
+             if($this->D()>0){     return 1;}
+             elseif($this->D()==0){return 0;}
+             elseif($this->D()<0) { return -1;}
+                      }//checkD
+  private function root(){ 
+	         if($this->p['b']!=0){ 
+			 exit;}
+	         elseif($this->p['c']!=0){ exit('The equation does not have a solution.');}
+	         else{ exit('The equation has infinity number of solutions. All numbers are the roots.');}
+	        }				  
+         
+   public function roots(){
+            switch ($this->checkD()) {
+            case 1: {$this->x[0]=(-$this->p['b']-sqrt($this->D()))/(2*$this->p['a']);
+                $this->x[1]=(-$this->p['b']+sqrt($this->D()))/(2*$this->p['a']);
+                    return $this->x;
+            }
+                        break;
+            case 0:{$this->x[0]=$this->x[1]=-$this->p['b']/(2*$this->p['a']);
+                    return $this->x;
+            }
+                        break;
+            case -1:{
+			         $this->x[0]=-$this->p['b']/(2*$this->p['a']);
+                     $this->x[1]=sqrt(-$this->D())/(2*$this->p['a']);
+                    return $this->x;
+            }
+                        break;
+            default:
+                        break;
+            }//switch    
+    
+            }//roots
+   
+  public function printReal(){
+ 
+           return $this->x;
+            }//printReal
+   
+  public function printComplex(){
+           if($this->x[1]>0){
+          
+            return $this->x;
+		   }
+   
+          }//printComplex
+       }//equation2
+
+
+if(isset($_GET['a'])){
+$a=$_GET['a'];//or =1
+$b=$_GET['b'];//or =3
+$c=$_GET['c'];//or =2
+$dec=$_GET['dec'];//or =4
+}else{
+$a=1; $b=2; $c=1;
+}
+$e=new equation2($a,$b,$c,$dec);
+$e->p['a'];
+$e->p['b'];
+$e->p['c'];
+$response = [];
+$e->roots();
+$response['type'] = $e->checkD();
+ switch($response['type']){
+    case 1: {
+	   $response['answer'] = $e->printReal();
+	  }
+        break;
+    case 0:{
+	     $response['answer'] =  $e->printReal();}
+	     break;
+    case -1:{
+	 $response['answer'] =  $e->printComplex();
+	}
+        break;
+	default:
+		break;
+}
+
+print_r(json_encode($response))
+?>
